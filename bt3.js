18439a80ff8c448c5ec5da19094d3cb02c58d935
fcdat1234 <fcdat1234@gmail.com>
2024-09-30 20:31:17 +0700

    Add files via upload

---
+++ bt3.js
@@ -0,0 +1,10 @@
+var a = prompt("Nhap a");
+var b = prompt("Nhap b");
+var c = prompt("Nhap a");
+if(a >b && a >c){
+    console.log("So lon nhat la" + " " +a);
+}else if( b>c){
+    console.log("So lon nhat la" + " " +b);
+}else{
+    console.log("So lon nhat la" + " " +c);
+}
